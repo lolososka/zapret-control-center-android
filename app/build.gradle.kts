b53c5e8449601fdc3b610a9ba827f249f3f300db
lolososka <116503803+lolososka@users.noreply.github.com>
2026-09-11 14:54:00 +0200

    chore(android): bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.lolososka.zapretmobile"
         minSdk = 21
         targetSdk = 34
-        versionCode = 11
-        versionName = "0.2.0"
+        versionCode = 12
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
